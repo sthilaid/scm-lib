5b97953c0adcfc9700b05b5917522bedddad4a96
dave@david-laptop <dave@david-laptop>
2009-02-12 14:16:03 -0500

    added queue define-type identifier. now multiple loading of the scm-lib will have compatible queues.

--- scm-lib.scm
+++ scm-lib.scm
@@ -407,8 +407,8 @@
 
 
 ;;; Queue implementation
-(define-type queue head tail size)
-(define-type queue-elem prev next value)
+(define-type queue id: queue head tail size)
+(define-type queue-elem id: queue-elem prev next value)
 (define (new-queue) (make-queue #f #f 0))
 
 (define (enqueue! q val)
